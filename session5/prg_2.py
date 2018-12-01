6cbd0a1d69625f043932f49aa646121b941b5853
vipinsachan <39883681+vipinsachan@users.noreply.github.com>
2018-12-01 18:48:01 +0530

    Add files via upload

---
+++ session5/prg_2.py
@@ -0,0 +1,4 @@
+d = {"id":101,"name":"ABC","dept":"HR","salary":45000}
+
+d['name'] = "XYZ"
+print(d)
---
+++ session5/prg_5.py
@@ -0,0 +1,7 @@
+d = {}
+
+for val in range(1,16):
+    d[val] = val**2
+    # d.setdefault(key,0)
+
+print(d)
